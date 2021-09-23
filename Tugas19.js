86ef8db17af27ef07f980844579782d4f1f73972
Albert <albert.bagil@gmail.com>
2021-09-23 14:51:09 +0700

    Tugas 19 Javascript
    Tugas 19 Javascript untuk dikumpulkan ke Niomic

---
+++ Tugas19.js
@@ -0,0 +1,16 @@
+function ulang() {
+  var repeat1 = 'Semangat ...'
+  console.log(repeat1.repeat(5));
+  console.log('');
+  console.log('*'.repeat(5));
+  console.log('*'.repeat(24));
+  console.log('*'.repeat(44));
+  console.log('*'.repeat(64));
+  console.log('*'.repeat(94));
+  console.log('*'.repeat(64));
+  console.log('*'.repeat(44));
+  console.log('*'.repeat(24));
+  console.log('*'.repeat(5));
+}
+
+ulang()
